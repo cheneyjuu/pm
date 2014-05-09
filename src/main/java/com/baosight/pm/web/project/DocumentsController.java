a14951710bcaed10f57c539549be495c7c20e613
cheneyjuu <cheneyjuu@163.com>
2014-05-09 21:33:35 +0800

    添加了测试Markdown的main方法

--- src/main/java/com/baosight/pm/web/project/DocumentsController.java
+++ src/main/java/com/baosight/pm/web/project/DocumentsController.java
@@ -1,7 +1,14 @@
 package com.baosight.pm.web.project;
 
+import org.pegdown.Extensions;
+import org.pegdown.Parser;
+import org.pegdown.PegDownProcessor;
+import org.pegdown.ast.RootNode;
+import org.pegdown.plugins.PegDownPlugins;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.*;
 
 /**
  * User: Juchen
@@ -25,5 +32,15 @@ public class DocumentsController {
     @RequestMapping (value = "details")
     public String details(){
         return "project/documentDetails";
+    }
+
+    public static void main(String[] args){
+        PegDownProcessor pegDownProcessor = new PegDownProcessor(Extensions.TABLES);
+
+        String markdownStr = ":-----|-----:|----|:---:\n" +
+                "Cool  | Shit | in | here";
+
+        String result = pegDownProcessor.markdownToHtml(markdownStr);
+        System.out.println(result);
     }
 }

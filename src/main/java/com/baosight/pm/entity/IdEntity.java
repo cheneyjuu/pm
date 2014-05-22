bf54acf96fcde509dc68efe6f51283b215a93846
巨臣 <juchen0416@gmail.com>
2014-05-22 16:04:00 +0800

    将主键生成策略改为AUTO（由程序控制）

--- src/main/java/com/baosight/pm/entity/IdEntity.java
+++ src/main/java/com/baosight/pm/entity/IdEntity.java
@@ -25,7 +25,7 @@ public abstract class IdEntity {
 	protected Long id;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	public Long getId() {
 		return id;
 	}
---
+++ src/main/java/com/baosight/pm/entity/Project.java
@@ -0,0 +1,9 @@
+package com.baosight.pm.entity;
+
+/**
+ * User: Juchen
+ * Date: 14-5-22
+ * Time: 14:34
+ */
+public class Project {
+}

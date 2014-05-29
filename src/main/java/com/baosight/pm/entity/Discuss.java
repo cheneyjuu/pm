3f6ecec24882ae32eae36bc2c9af4b6c27a4adbe
巨臣 <juchen0416@gmail.com>
2014-05-29 15:33:31 +0800

    上传配置
    配置文件上传解析器

---
+++ src/main/java/com/baosight/pm/entity/Discuss.java
@@ -0,0 +1,9 @@
+package com.baosight.pm.entity;
+
+/**
+ * User: Juchen
+ * Date: 14-5-29
+ * Time: 10:40
+ */
+public class Discuss {
+}
---
+++ src/main/java/com/baosight/pm/utility/HandlerUpload.java
@@ -0,0 +1,9 @@
+package com.baosight.pm.utility;
+
+/**
+ * User: Juchen
+ * Date: 14-5-29
+ * Time: 14:38
+ */
+public class HandlerUpload {
+}

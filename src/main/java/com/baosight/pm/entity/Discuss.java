62892febc5bf17bf9e9666161a13eef46bb6262e
巨臣 <juchen0416@gmail.com>
2014-05-29 15:37:33 +0800

    上传文件
    添加上传文件方法

--- src/main/java/com/baosight/pm/entity/Discuss.java
+++ src/main/java/com/baosight/pm/entity/Discuss.java
@@ -1,9 +1,67 @@
 package com.baosight.pm.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
 /**
  * User: Juchen
  * Date: 14-5-29
  * Time: 10:40
  */
+@Entity
+@Table(name = "tbl_discuss")
 public class Discuss {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "id")
+    private String id;
+    private String title;
+    private String content;
+    private String createTime;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
 }
--- src/main/java/com/baosight/pm/web/project/DiscussController.java
+++ src/main/java/com/baosight/pm/web/project/DiscussController.java
@@ -1,7 +1,16 @@
 package com.baosight.pm.web.project;
 
+import com.baosight.pm.utility.HandlerUpload;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.util.Map;
 
 /**
  * User: Juchen
@@ -15,5 +24,17 @@ public class DiscussController {
     @RequestMapping (value = "index")
     public String index(){
         return "discuss/discussForm";
+    }
+
+    @RequestMapping (value = "/upload")
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile[] files,
+                         HttpServletRequest request, Model model){
+        Map<String, String> fileNameAndPathMap = null;
+        HandlerUpload handlerUpload = new HandlerUpload();
+        for (MultipartFile file : files){
+            fileNameAndPathMap = handlerUpload.uploadFile(file,request);
+        }
+        return  fileNameAndPathMap.get("filePath")+fileNameAndPathMap.get("fileName");
     }
 }
